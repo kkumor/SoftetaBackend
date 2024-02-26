e2ba585987ffbb603b5edda95b481936ec8f8dba
kkumor <kumor.kam@gmail.com>
2024-02-26 23:32:16 +0100

    fix validator tests

--- Claims.Tests/Controllers/Validators/AddCoverDtoValidator.cs
+++ Claims.Tests/Controllers/Validators/AddCoverDtoValidator.cs
@@ -1,25 +1,44 @@
 ﻿using Claims.Api.Controllers.Model;
 using Claims.Api.Controllers.Validators;
+using FluentAssertions;
 
 namespace Claims.Tests.Controllers.Validators;
 
-public class AddCoverDtoValidator : IValidator<AddCoverDto>
+public class AddCoverDtoValidatorTests
 {
-    public Task<ValidatorResult> Validate(AddCoverDto inputModel)
+    private readonly AddCoverDtoValidator _validator = new();
+
+    [Fact]
+    public async Task ValidateShouldReturnErrorWhenStartDateIsInThePast()
     {
-        const int daysInYear = 365;
-        var coverPeriod = inputModel.EndDate.DayNumber - inputModel.StartDate.DayNumber;
-        var errors = new List<string>();
-        if (inputModel.StartDate < DateOnly.FromDateTime(DateTime.Today))
+        // Arrange
+        var inputModel = new AddCoverDto
         {
-            errors.Add("StartDate cannot be in the past");
-        }
+            StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+            EndDate = DateOnly.FromDateTime(DateTime.Today)
+        };
 
-        if (coverPeriod > daysInYear)
+        // Act
+        var result = await _validator.Validate(inputModel);
+
+        // Assert
+        result.Errors.Should().Contain(error => error.Contains("StartDate cannot be in the past"));
+    }
+
+    [Fact]
+    public async Task ValidateShouldReturnErrorWhenCoverPeriodExceedsOneYear()
+    {
+        // Arrange
+        var inputModel = new AddCoverDto
         {
-            errors.Add("Total insurance period cannot exceed 1 year");
-        }
+            StartDate = DateOnly.FromDateTime(DateTime.Today),
+            EndDate = DateOnly.FromDateTime(DateTime.Today.AddYears(1).AddDays(1))
+        };
 
-        return Task.FromResult(new ValidatorResult(errors));
+        // Act
+        var result = await _validator.Validate(inputModel);
+
+        // Assert
+        result.Errors.Should().Contain(error => error.Contains("Total insurance period cannot exceed 1 year"));
     }
 }
